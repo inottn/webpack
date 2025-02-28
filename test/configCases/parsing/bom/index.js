b7b66944c50484e2127e2b091980963eccf9b905
inottn <inottn@outlook.com>
2025-02-28 23:17:52 +0800

    feat: add removeBOM utility to handle BOM in source strings

---
+++ test/configCases/parsing/bom/index.js
@@ -0,0 +1,16 @@
+﻿import "./index.css";
+
+it("should not contain BOM", async () => {
+	const fs = __non_webpack_require__("fs");
+	const path = __non_webpack_require__("path");
+	const js = await fs.promises.readFile(
+		path.resolve(__dirname, "bundle0.js"),
+		"utf-8"
+	);
+	const css = await fs.promises.readFile(
+		path.resolve(__dirname, "bundle0.css"),
+		"utf-8"
+	);
+	expect(js).not.toContain("\uFEFF");
+	expect(css).not.toContain("\uFEFF");
+});
---
+++ test/configCases/parsing/bom/webpack.config.js
@@ -0,0 +1,11 @@
+/** @type {import("../../../../").Configuration} */
+module.exports = {
+	target: "web",
+	node: false,
+	entry: {
+		main: "./index.js"
+	},
+	experiments: {
+		css: true
+	}
+};
